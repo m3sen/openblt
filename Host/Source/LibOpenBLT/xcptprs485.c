9eaebaa62c163b3bded942edaea6d76d9a753569
kmshim <mwtkmshim@gmail.com>
2020-11-17 09:50:40 +0900

    File mode is changed 644 to 755 except *485 files, Added new *485 files

--- Host/Source/LibOpenBLT/xcptprs485.c
+++ Host/Source/LibOpenBLT/xcptprs485.c
@@ -1,7 +1,7 @@
 /************************************************************************************//**
-* \file         xcptpuart.c
-* \brief        XCP UART transport layer source file.
-* \ingroup      XcpTpUart
+* \file         xcptprs485.c
+* \brief        XCP RS485 transport layer source file.
+* \ingroup      XcpTpRs485
 * \internal
 *----------------------------------------------------------------------------------------
 *                          C O P Y R I G H T
@@ -37,7 +37,7 @@
 #include <string.h>                         /* for string library                      */
 #include "session.h"                        /* Communication session module            */
 #include "xcploader.h"                      /* XCP loader module                       */
-#include "xcptpuart.h"                      /* XCP UART transport layer                */
+#include "xcptprs485.h"                     /* XCP RS485 transport layer               */
 #include "util.h"                           /* Utility module                          */
 #include "serialport.h"                     /* Serial port module                      */
 
@@ -45,11 +45,11 @@
 /****************************************************************************************
 * Function prototypes
 ****************************************************************************************/
-static void XcpTpUartInit(void const * settings);
-static void XcpTpUartTerminate(void);
-static bool XcpTpUartConnect(void);
-static void XcpTpUartDisconnect(void);
-static bool XcpTpUartSendPacket(tXcpTransportPacket const * txPacket, 
+static void XcpTpRs485Init(void const * settings);
+static void XcpTpRs485Terminate(void);
+static bool XcpTpRs485Connect(void);
+static void XcpTpRs485Disconnect(void);
+static bool XcpTpRs485SendPacket(tXcpTransportPacket const * txPacket, 
                                 tXcpTransportPacket * rxPacket, uint16_t timeout);
 
  
@@ -57,13 +57,13 @@ static bool XcpTpUartSendPacket(tXcpTransportPacket const * txPacket,
 * Local constant declarations
 ****************************************************************************************/
 /** \brief XCP transport layer structure filled with UART specifics. */
-static const tXcpTransport uartTransport =
+static const tXcpTransport rs485Transport =
 {
-  XcpTpUartInit,
-  XcpTpUartTerminate,
-  XcpTpUartConnect,
-  XcpTpUartDisconnect,
-  XcpTpUartSendPacket
+  XcpTpRs485Init,
+  XcpTpRs485Terminate,
+  XcpTpRs485Connect,
+  XcpTpRs485Disconnect,
+  XcpTpRs485SendPacket
 };
 
 
@@ -71,7 +71,7 @@ static const tXcpTransport uartTransport =
 * Local data declarations
 ****************************************************************************************/
 /** \brief The settings to use in this transport layer. */
-static tXcpTpUartSettings tpUartSettings;
+static tXcpTpRs485Settings tpRs485Settings;
 
 
 /***********************************************************************************//**
@@ -80,9 +80,9 @@ static tXcpTpUartSettings tpUartSettings;
 ** \return    Pointer to transport layer structure.
 **
 ****************************************************************************************/
-tXcpTransport const * XcpTpUartGetTransport(void)
+tXcpTransport const * XcpTpRs485GetTransport(void)
 {
-  return &uartTransport;
+  return &rs485Transport;
 } /*** end of XcpTpUartGetTransport ***/
 
 
@@ -92,13 +92,13 @@ tXcpTransport const * XcpTpUartGetTransport(void)
 ** \return    None.
 **
 ****************************************************************************************/
-static void XcpTpUartInit(void const * settings)
+static void XcpTpRs485Init(void const * settings)
 {
   char * uartPortName;
 
   /* Reset transport layer settings. */
-  tpUartSettings.portname = NULL;
-  tpUartSettings.baudrate = 0;
+  tpRs485Settings.portname = NULL;
+  tpRs485Settings.baudrate = 0;
   
   /* Check parameters. */
   assert(settings != NULL);
@@ -107,19 +107,19 @@ static void XcpTpUartInit(void const * settings)
   if (settings != NULL) /*lint !e774 */
   {
     /* Shallow copy the transport layer settings for layer usage. */
-    tpUartSettings = *((tXcpTpUartSettings *)settings);
+    tpRs485Settings = *((tXcpTpRs485Settings *)settings);
     /* The portname is a pointer and it is not guaranteed that it stays valid so we need
      * to deep copy this one. note the +1 for '\0' in malloc.
      */
-    assert(((tXcpTpUartSettings *)settings)->portname != NULL);
-    if (((tXcpTpUartSettings *)settings)->portname != NULL) /*lint !e774 */
+    assert(((tXcpTpRs485Settings *)settings)->portname != NULL);
+    if (((tXcpTpRs485Settings *)settings)->portname != NULL) /*lint !e774 */
     {
-      uartPortName = malloc(strlen(((tXcpTpUartSettings *)settings)->portname) + 1);
+      uartPortName = malloc(strlen(((tXcpTpRs485Settings *)settings)->portname) + 1);
       assert(uartPortName != NULL);
       if (uartPortName != NULL) /*lint !e774 */
       {
-        strcpy(uartPortName, ((tXcpTpUartSettings *)settings)->portname);
-        tpUartSettings.portname = uartPortName;
+        strcpy(uartPortName, ((tXcpTpRs485Settings *)settings)->portname);
+        tpRs485Settings.portname = uartPortName;
       }
     }
   }
@@ -132,18 +132,18 @@ static void XcpTpUartInit(void const * settings)
 ** \brief     Terminates the transport layer.
 **
 ****************************************************************************************/
-static void XcpTpUartTerminate(void)
+static void XcpTpRs485Terminate(void)
 {
   /* Terminate the serial port. */
   SerialPortTerminate();
   /* Release memory that was allocated for storing the port name. */
-  if (tpUartSettings.portname != NULL)
+  if (tpRs485Settings.portname != NULL)
   {
-    free((char *)tpUartSettings.portname);
+    free((char *)tpRs485Settings.portname);
   }
   /* Reset transport layer settings. */
-  tpUartSettings.portname = NULL;
-  tpUartSettings.baudrate = 0;
+  tpRs485Settings.portname = NULL;
+  tpRs485Settings.baudrate = 0;
 } /*** end of XcpTpUartTerminate ***/
 
 
@@ -152,28 +152,28 @@ static void XcpTpUartTerminate(void)
 ** \return    True is connected, false otherwise.
 **
 ****************************************************************************************/
-static bool XcpTpUartConnect(void)
+static bool XcpTpRs485Connect(void)
 {
   bool result = false;
   bool baudrateSupported;
   tSerialPortBaudrate baudrate;
   
   /* Check if the specified baudrate is supported by the serial port driver. */
-  baudrateSupported = (tpUartSettings.baudrate == 9600) || 
-                      (tpUartSettings.baudrate == 19200) ||
-                      (tpUartSettings.baudrate == 38400) ||
-                      (tpUartSettings.baudrate == 57600) ||
-                      (tpUartSettings.baudrate == 115200);
+  baudrateSupported = (tpRs485Settings.baudrate == 9600) || 
+                      (tpRs485Settings.baudrate == 19200) ||
+                      (tpRs485Settings.baudrate == 38400) ||
+                      (tpRs485Settings.baudrate == 57600) ||
+                      (tpRs485Settings.baudrate == 115200);
                       
   /* Check transport layer settings. */
-  assert(tpUartSettings.portname != NULL);
+  assert(tpRs485Settings.portname != NULL);
   assert(baudrateSupported);
                       
   /* Only continue if the transport layer settings are valid. */
-  if ( (tpUartSettings.portname != NULL) && (baudrateSupported) ) /*lint !e774 */
+  if ( (tpRs485Settings.portname != NULL) && (baudrateSupported) ) /*lint !e774 */
   {
     /* Convert the numeric baudrate to the one supported by the serial port driver. */
-    switch (tpUartSettings.baudrate) 
+    switch (tpRs485Settings.baudrate)
     {
       case 115200:
         baudrate = SERIALPORT_BR115200;
@@ -193,7 +193,7 @@ static bool XcpTpUartConnect(void)
         break;
     }
     /* Connect to the serial port. */
-    result = SerialPortOpen(tpUartSettings.portname, baudrate);
+    result = SerialPortOpen(tpRs485Settings.portname, baudrate);
   }
   /* Give the result back to the caller. */
   return result;
@@ -204,12 +204,76 @@ static bool XcpTpUartConnect(void)
 ** \brief     Disconnects from the transport layer.
 **
 ****************************************************************************************/
-static void XcpTpUartDisconnect(void)
+static void XcpTpRs485Disconnect(void)
 {
   /* Disconnect from the serial port. */
   SerialPortClose();
 } /*** end of XcpTpUartDisconnect ***/
 
+static const uint8_t aucCRCHi[] = {
+   0x00, 0xC1, 0x81, 0x40, 0x01, 0xC0, 0x80, 0x41, 0x01, 0xC0, 0x80, 0x41,
+   0x00, 0xC1, 0x81, 0x40, 0x01, 0xC0, 0x80, 0x41, 0x00, 0xC1, 0x81, 0x40,
+   0x00, 0xC1, 0x81, 0x40, 0x01, 0xC0, 0x80, 0x41, 0x01, 0xC0, 0x80, 0x41,
+   0x00, 0xC1, 0x81, 0x40, 0x00, 0xC1, 0x81, 0x40, 0x01, 0xC0, 0x80, 0x41,
+   0x00, 0xC1, 0x81, 0x40, 0x01, 0xC0, 0x80, 0x41, 0x01, 0xC0, 0x80, 0x41,
+   0x00, 0xC1, 0x81, 0x40, 0x01, 0xC0, 0x80, 0x41, 0x00, 0xC1, 0x81, 0x40,
+   0x00, 0xC1, 0x81, 0x40, 0x01, 0xC0, 0x80, 0x41, 0x00, 0xC1, 0x81, 0x40,
+   0x01, 0xC0, 0x80, 0x41, 0x01, 0xC0, 0x80, 0x41, 0x00, 0xC1, 0x81, 0x40,
+   0x00, 0xC1, 0x81, 0x40, 0x01, 0xC0, 0x80, 0x41, 0x01, 0xC0, 0x80, 0x41,
+   0x00, 0xC1, 0x81, 0x40, 0x01, 0xC0, 0x80, 0x41, 0x00, 0xC1, 0x81, 0x40,
+   0x00, 0xC1, 0x81, 0x40, 0x01, 0xC0, 0x80, 0x41, 0x01, 0xC0, 0x80, 0x41,
+   0x00, 0xC1, 0x81, 0x40, 0x00, 0xC1, 0x81, 0x40, 0x01, 0xC0, 0x80, 0x41,
+   0x00, 0xC1, 0x81, 0x40, 0x01, 0xC0, 0x80, 0x41, 0x01, 0xC0, 0x80, 0x41,
+   0x00, 0xC1, 0x81, 0x40, 0x00, 0xC1, 0x81, 0x40, 0x01, 0xC0, 0x80, 0x41,
+   0x01, 0xC0, 0x80, 0x41, 0x00, 0xC1, 0x81, 0x40, 0x01, 0xC0, 0x80, 0x41,
+   0x00, 0xC1, 0x81, 0x40, 0x00, 0xC1, 0x81, 0x40, 0x01, 0xC0, 0x80, 0x41,
+   0x00, 0xC1, 0x81, 0x40, 0x01, 0xC0, 0x80, 0x41, 0x01, 0xC0, 0x80, 0x41,
+   0x00, 0xC1, 0x81, 0x40, 0x01, 0xC0, 0x80, 0x41, 0x00, 0xC1, 0x81, 0x40,
+   0x00, 0xC1, 0x81, 0x40, 0x01, 0xC0, 0x80, 0x41, 0x01, 0xC0, 0x80, 0x41,
+   0x00, 0xC1, 0x81, 0x40, 0x00, 0xC1, 0x81, 0x40, 0x01, 0xC0, 0x80, 0x41,
+   0x00, 0xC1, 0x81, 0x40, 0x01, 0xC0, 0x80, 0x41, 0x01, 0xC0, 0x80, 0x41,
+   0x00, 0xC1, 0x81, 0x40
+};
+
+static const uint8_t aucCRCLo[] = {
+    0x00, 0xC0, 0xC1, 0x01, 0xC3, 0x03, 0x02, 0xC2, 0xC6, 0x06, 0x07, 0xC7,
+    0x05, 0xC5, 0xC4, 0x04, 0xCC, 0x0C, 0x0D, 0xCD, 0x0F, 0xCF, 0xCE, 0x0E,
+    0x0A, 0xCA, 0xCB, 0x0B, 0xC9, 0x09, 0x08, 0xC8, 0xD8, 0x18, 0x19, 0xD9,
+    0x1B, 0xDB, 0xDA, 0x1A, 0x1E, 0xDE, 0xDF, 0x1F, 0xDD, 0x1D, 0x1C, 0xDC,
+    0x14, 0xD4, 0xD5, 0x15, 0xD7, 0x17, 0x16, 0xD6, 0xD2, 0x12, 0x13, 0xD3,
+    0x11, 0xD1, 0xD0, 0x10, 0xF0, 0x30, 0x31, 0xF1, 0x33, 0xF3, 0xF2, 0x32,
+    0x36, 0xF6, 0xF7, 0x37, 0xF5, 0x35, 0x34, 0xF4, 0x3C, 0xFC, 0xFD, 0x3D,
+    0xFF, 0x3F, 0x3E, 0xFE, 0xFA, 0x3A, 0x3B, 0xFB, 0x39, 0xF9, 0xF8, 0x38,
+    0x28, 0xE8, 0xE9, 0x29, 0xEB, 0x2B, 0x2A, 0xEA, 0xEE, 0x2E, 0x2F, 0xEF,
+    0x2D, 0xED, 0xEC, 0x2C, 0xE4, 0x24, 0x25, 0xE5, 0x27, 0xE7, 0xE6, 0x26,
+    0x22, 0xE2, 0xE3, 0x23, 0xE1, 0x21, 0x20, 0xE0, 0xA0, 0x60, 0x61, 0xA1,
+    0x63, 0xA3, 0xA2, 0x62, 0x66, 0xA6, 0xA7, 0x67, 0xA5, 0x65, 0x64, 0xA4,
+    0x6C, 0xAC, 0xAD, 0x6D, 0xAF, 0x6F, 0x6E, 0xAE, 0xAA, 0x6A, 0x6B, 0xAB,
+    0x69, 0xA9, 0xA8, 0x68, 0x78, 0xB8, 0xB9, 0x79, 0xBB, 0x7B, 0x7A, 0xBA,
+    0xBE, 0x7E, 0x7F, 0xBF, 0x7D, 0xBD, 0xBC, 0x7C, 0xB4, 0x74, 0x75, 0xB5,
+    0x77, 0xB7, 0xB6, 0x76, 0x72, 0xB2, 0xB3, 0x73, 0xB1, 0x71, 0x70, 0xB0,
+    0x50, 0x90, 0x91, 0x51, 0x93, 0x53, 0x52, 0x92, 0x96, 0x56, 0x57, 0x97,
+    0x55, 0x95, 0x94, 0x54, 0x9C, 0x5C, 0x5D, 0x9D, 0x5F, 0x9F, 0x9E, 0x5E,
+    0x5A, 0x9A, 0x9B, 0x5B, 0x99, 0x59, 0x58, 0x98, 0x88, 0x48, 0x49, 0x89,
+    0x4B, 0x8B, 0x8A, 0x4A, 0x4E, 0x8E, 0x8F, 0x4F, 0x8D, 0x4D, 0x4C, 0x8C,
+    0x44, 0x84, 0x85, 0x45, 0x87, 0x47, 0x46, 0x86, 0x82, 0x42, 0x43, 0x83,
+    0x41, 0x81, 0x80, 0x40
+};
+
+uint16_t ModbusCRC16(uint8_t* pucFrame, uint16_t usLen)
+{
+    uint8_t ucCRCHi = 0xFF;
+    uint8_t ucCRCLo = 0xFF;
+    int iIndex;
+
+    while (usLen--)
+    {
+        iIndex = ucCRCLo ^ *(pucFrame++);
+        ucCRCLo = (uint8_t)(ucCRCHi ^ aucCRCHi[iIndex]);
+        ucCRCHi = aucCRCLo[iIndex];
+    }
+    return (uint16_t)(ucCRCHi << 8 | ucCRCLo);
+}
 
 /************************************************************************************//**
 ** \brief     Transmits an XCP packet on the transport layer and attempts to receive the
@@ -221,7 +285,7 @@ static void XcpTpUartDisconnect(void)
 ** \return    True is successful and a response packet was received, false otherwise.
 **
 ****************************************************************************************/
-static bool XcpTpUartSendPacket(tXcpTransportPacket const * txPacket, 
+static bool XcpTpRs485SendPacket(tXcpTransportPacket const * txPacket,
                                 tXcpTransportPacket * rxPacket, uint16_t timeout)
 {
   bool result = false;
@@ -229,7 +293,7 @@ static bool XcpTpUartSendPacket(tXcpTransportPacket const * txPacket,
   /* uartBuffer is static to lower the stack load. +1 because the first byte for an XCP 
    * packet on the UART transport layer contains the packet length.
    */
-  static uint8_t uartBuffer[XCPLOADER_PACKET_SIZE_MAX + 1]; 
+  static uint8_t rs485Buffer[XCPLOADER_PACKET_SIZE_MAX + 9]; 
   uint32_t responseTimeoutTime = 0;
   bool packetReceptionComplete;
 
@@ -246,13 +310,28 @@ static bool XcpTpUartSendPacket(tXcpTransportPacket const * txPacket,
      * the XCP packet data but just the length of the packet is added to the first 
      * byte.
      */
-    uartBuffer[0] = txPacket->len;
+    // Slave Address: Function : code : Data Data Data : CRC  Lo : CRC Hi 
+    rs485Buffer[0] = (uint8_t)tpRs485Settings.SlaveAddr;
+    rs485Buffer[1] = (uint8_t)tpRs485Settings.FuncCode; // holding register
+    rs485Buffer[2] = (uint8_t)((tpRs485Settings.RegAddr>>8) & 0xFF); //Holding register address Hi
+    rs485Buffer[3] = (uint8_t)(tpRs485Settings.RegAddr & 0xFF); //Holdign register address Lo
+    rs485Buffer[4] = 0x00; // word(16bit) length hi
+    rs485Buffer[5] = (txPacket->len+1)/2; // word(16bit) length lo 
+    rs485Buffer[6] = txPacket->len; // byte(8bit) length (= word length * 2)
+    // data from 7
+	
     for (byteIdx=0; byteIdx<txPacket->len; byteIdx++)
     {
-      uartBuffer[byteIdx + 1] = txPacket->data[byteIdx];
+        rs485Buffer[byteIdx + 7] = txPacket->data[byteIdx];
     }
+
+    uint16_t ui16Crc = ModbusCRC16(rs485Buffer, txPacket->len +7);
+
+    rs485Buffer[txPacket->len + 7] = (uint8_t)(ui16Crc & 0xFF); //0xA5; //CRC low byte
+    rs485Buffer[txPacket->len + 8] = (uint8_t)((ui16Crc >> 8) & 0xff); // 0xA5; //CRC high byte
+
     /* Transmit the packet. */
-    if (!SerialPortWrite(uartBuffer, txPacket->len + 1))
+    if (!SerialPortWrite(rs485Buffer, txPacket->len + 9))
     {
       result = false;
     }
--- Host/Source/LibOpenBLT/xcptprs485.h
+++ Host/Source/LibOpenBLT/xcptprs485.h
@@ -1,7 +1,7 @@
 /************************************************************************************//**
-* \file         xcptpnet.h
-* \brief        XCP TCP/IP transport layer header file.
-* \ingroup      XcpTpNet
+* \file         xcptprs485.h
+* \brief        XCP Rs495 transport layer header file.
+* \ingroup      XcpTpRs485
 * \internal
 *----------------------------------------------------------------------------------------
 *                          C O P Y R I G H T
@@ -26,12 +26,12 @@
 * \endinternal
 ****************************************************************************************/
 /************************************************************************************//**
-* \defgroup   XcpTpNet XCP TCP/IP transport layer
-* \brief      This module implements the XCP transport layer for TCP/IP.
+* \defgroup   XcpTpUart XCP UART transport layer
+* \brief      This module implements the XCP transport layer for UART.
 * \ingroup    XcpLoader
 ****************************************************************************************/
-#ifndef XCPTPNET_H
-#define XCPTPNET_H
+#ifndef XCPTPRS485_H
+#define XCPTPRS485_H
 
 #ifdef __cplusplus
 extern "C" {
@@ -41,23 +41,27 @@ extern "C" {
 * Type definitions
 ****************************************************************************************/
 /** \brief Layout of structure with settings specific to the XCP transport layer module
- *         for TCP/IP.
+ *         for UART.
  */
-typedef struct t_xcp_tp_net_settings
+typedef struct t_xcp_tp_rs485_settings
 {
-  char const * address;          /**< Target IP-address or hostname on the network.    */
-  uint16_t port;                 /**< TCP port to use.                                 */
-} tXcpTpNetSettings;
+  char const * portname;               /**< Interface port name, i.e. /dev/ttyUSB0.    */
+  uint32_t baudrate;                   /**< Communication speed in bits/sec.           */
+  uint32_t SlaveAddr;                  /**< Slave Address, scanf를 사용함으로 입력변수 처리를 위해 32bit로 선언함. */
+  uint32_t FuncCode;                   /**< Set to 16    , scanf를 사용함으로 입력변수 처리를 위해 32bit로 선언함. */
+  uint32_t RegAddr;                    /**< Set to 0x1f  , scanf를 사용함으로 입력변수 처리를 위해 32bit로 선언함. */
+  uint32_t CRC16;                      /**< Set to CRC16 , scanf를 사용함으로 입력변수 처리를 위해 32bit로 선언함. */
+} tXcpTpRs485Settings;
 
 
 /***************************************************************************************
 * Function prototypes
 ****************************************************************************************/
-tXcpTransport const * XcpTpNetGetTransport(void);
+tXcpTransport const * XcpTpRs485GetTransport(void);
 
 #ifdef __cplusplus
 }
 #endif
 
-#endif /* XCPTPNET_H */
-/*********************************** end of xcptpnet.h *********************************/
+#endif /* XCPTPRS485_H */
+/*********************************** end of xcptpuart.h ********************************/
